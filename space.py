01169e916aa8686a39ef976fe5aecf075604c288
Ilya Voytov <ilya@voytov.com>
2021-02-10 16:08:04 -0500

    Fully working implementation using SQLite as backend

--- space.py
+++ space.py
@@ -3,51 +3,75 @@ import sqlite3 as sq3
 from typing import Tuple
 from haversine import haversine_vector
 
-def read_data() -> pd.DataFrame:
-    df = pd.read_json('starlink_historical_data.json')
-    creation_date = pd.to_datetime(df.spaceTrack.apply(lambda x: x['CREATION_DATE']))
-    df = pd.concat([df[['id', 'longitude', 'latitude']], creation_date], axis=1).set_index('spaceTrack').sort_index().rename(columns={'id': 'sat_id'}).dropna()
-    return df
 
-"""
-:sat_id: satellite id
-:time: will return last known position before or at this time
-Returns last known position as tuple of [lat, long]
-"""
-def get_last_position(df, sat_id: str, datetime: pd.Timestamp = pd.Timestamp.now()) -> Tuple[float, float]:
+def read_data() -> pd.DataFrame:
+    """
+    Reads data and stores in the SQL database.
+    Any rows where either the Long or the Lat is Null will be omitted
+    """
+    df = pd.read_json("starlink_historical_data.json")
+    creation_date = pd.to_datetime(df.spaceTrack.apply(lambda x: x["CREATION_DATE"]))
+    df = (
+        pd.concat([df[["id", "longitude", "latitude"]], creation_date], axis=1)
+        .set_index("spaceTrack")
+        .sort_index()
+        .rename(columns={"id": "sat_id"})
+        .dropna()
+    )
+    con = sq3.connect("space.db")
+    df.to_sql("space", con=con, if_exists="replace")
+    return con
+
+
+def get_last_position(
+    con, sat_id: str, datetime: pd.Timestamp = pd.Timestamp.now()
+) -> Tuple[float, float]:
+    """
+    :con: SQL connection
+    :sat_id: satellite id
+    :time: will return last known position before or at this time
+    Returns last known position as tuple of [lat, long]
+    """
+    df = pd.read_sql(
+        "SELECT * FROM space", con=con, index_col="spaceTrack", parse_dates="spaceTrack"
+    )
     out = df[(df.sat_id == sat_id) & (df.index <= datetime)].dropna()
     if len(out):
         return (out.iloc[-1].latitude, out.iloc[-1].longitude)
     else:
         return None
 
-"""
-:df:
-:coord: coordinates in (lat,long) format
-:datetime: time when you are looking for the satellite
-returns None if no satellites found or id of the sat
-"""
-def get_closest_sat(df, coord: Tuple[float, float], datetime: pd.Timestamp = pd.Timestamp.now()) -> Tuple[str, float]:
+
+def get_closest_sat(
+    con, coord: Tuple[float, float], datetime: pd.Timestamp = pd.Timestamp.now()
+) -> Tuple[str, float]:
+    df = pd.read_sql(
+        "SELECT * FROM space", con=con, index_col="spaceTrack", parse_dates="spaceTrack"
+    )
+    """
+    :con: SQL connection
+    :coord: coordinates in (lat,long) format
+    :datetime: time when you are looking for the satellite
+    returns None if no satellites found or id of the sat
+    """
     if datetime not in df.index:
         raise KeyError("No information available for specified datetime")
 
-    sats = df.loc[datetime].set_index('sat_id')
+    sats = df.loc[datetime].set_index("sat_id")
     if not len(sats):
         return None
 
-    sats = sats[['latitude', 'longitude']] # reorder cols
-    dists = pd.Series(haversine_vector([coord]*len(sats), sats.values), index=sats.index)
+    sats = sats[["latitude", "longitude"]]  # reorder cols
+    dists = pd.Series(
+        haversine_vector([coord] * len(sats), sats.values), index=sats.index
+    )
     return dists.idxmin(), dists.min()
-    
-    
+
 
 def main():
     df = read_data()
-    get_last_position(df, '5eefa85e6527ee0006dcee24', pd.Timestamp.now())
-    
-
+    get_last_position(df, "5eefa85e6527ee0006dcee24", pd.Timestamp.now())
 
 
 if __name__ == "__main__":
     main()
-
--- tests.py
+++ tests.py
@@ -5,40 +5,43 @@ from space import *
 
 class SpaceTest(unittest.TestCase):
     def setUp(self):
-        self.df = read_data()
+        self.con = read_data()
 
     def test_data_import(self):
-        self.assertEqual(self.df.shape, (3027, 3))
+        df = pd.read_sql(
+            "SELECT * FROM space", con=self.con, index_col="spaceTrack", parse_dates="spaceTrack"
+        )
+        self.assertEqual(df.shape, (3027, 3))
 
     def test_bad_sat_id(self):
-        out = get_last_position(self.df, 'UFO')
+        out = get_last_position(self.con, 'UFO')
         self.assertIsNone(out)
 
     def test_sat_never_appears(self):
-        out = get_last_position(self.df, '5eefa85e6527ee0006dcee24', pd.Timestamp.now())
+        out = get_last_position(self.con, '5eefa85e6527ee0006dcee24', pd.Timestamp.now())
         self.assertIsNone(out)
 
     def test_time_exactly(self):
         # time is exactly the time of an appearance
-        out = get_last_position(self.df, '5f36cb59bd88830006274090', pd.Timestamp('2021-01-26 06:26:10'))
+        out = get_last_position(self.con, '5f36cb59bd88830006274090', pd.Timestamp('2021-01-26 06:26:10'))
         self.assertEqual(out, (-42.09235170986195, 94))
 
     def test_time_too_eary(self):
         # time is one second before first appearance
-        out = get_last_position(self.df, '5f36cb59bd8883000627409e', pd.Timestamp('2021-01-21 06:26:09'))
+        out = get_last_position(self.con, '5f36cb59bd8883000627409e', pd.Timestamp('2021-01-21 06:26:09'))
         self.assertIsNone(out)
 
 
     def test_dist_reg(self):
         # almost exact location
         coord = (-42.7, 67.0)
-        sat_id, dist = get_closest_sat(self.df, coord, pd.Timestamp('2021-01-26 14:26:10'))
+        sat_id, dist = get_closest_sat(self.con, coord, pd.Timestamp('2021-01-26 14:26:10'))
         self.assertEqual(sat_id, '5f889669c86e27000615b262')
 
     def test_bad_sat(self):
         # uses today's date, when no data is available
         coord = (-42.7, 67.0)
-        self.assertRaises(KeyError, get_closest_sat, self.df, coord)
+        self.assertRaises(KeyError, get_closest_sat, self.con, coord)
 
 if __name__ == '__main__':
     unittest.main()
